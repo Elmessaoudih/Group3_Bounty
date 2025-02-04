f99a3ebb87fc1324785fc46aa8045998ede7a572
Zeplyn <kelleyz22@students.ecu.edu>
2025-02-04 18:46:20 -0500

    Z

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,14 +6,40 @@ public class Timer : MonoBehaviour
 {
 
     [SerializeField] TextMeshProUGUI timerText;
-    float elapsedTime;
+    // Change this number to change how much time you have.
+    float remainingTime = 30f;
+
     // Update is called once per frame
     void Update()
     {
-        // This is a timer to count how long it takes for you to beat the game
-        elapsedTime += Time.deltaTime;
-        int minutes = Mathf.FloorToInt(elapsedTime / 60);
-        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        // This is a timer to count down how long you have left.
+
+        remainingTime -= Time.deltaTime;
+
+        if (remainingTime < 1)
+        {
+            remainingTime = 0;
+            LoseScreen();
+            enabled = false;
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void AddTime(float amount)
+    {
+        remainingTime += amount;
+    }
+
+    void LoseScreen()
+    {
+        Time.timeScale = 0f;
+
+        // The code for losing goes here.
+
+        // inventory.gameObject.SetActive(false);
+        // loseImage.SetActive(true);
     }
 }
